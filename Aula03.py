865b131e0765ef145a2b34760d01b1d310376b56
Lucas7yuri <140767646+Lucas7yuri@users.noreply.github.com>
2025-03-25 20:37:15 -0400

    Add files via upload

---
+++ Aula03.py
@@ -0,0 +1,9 @@
+#1. crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int (input('Digite um numero: '))
+n2 = int (input('Digite outro numero: '))
+s = n1 + n2 
+print(format(s))
+
+
+ 
